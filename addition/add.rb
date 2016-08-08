369419101b752d3c78bc53ab87b165f64cb1f151
Carrison57 <calebarrison@gmail.com>
2016-08-08 10:39:51 -0400

    ran second test

--- addition/add.rb
+++ addition/add.rb
@@ -1,3 +1,4 @@
 def my_addition(num1, num2)
-		0
+		num1 + num2
+
 	end
--- addition/test_add.rb
+++ addition/test_add.rb
@@ -8,6 +8,11 @@ class TestAddition < Minitest::Test
 		assert_equal(0, my_addition(0, 0))
 	end
 
+	def test_1_add_9_equals_10
+		assert_equal(10, my_addition(1, 9))
+	end
+
+
 
 
 end
