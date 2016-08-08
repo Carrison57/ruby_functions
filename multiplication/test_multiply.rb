fa5dc3775d3b698ad9540d974ccc423660f6521f
Carrison57 <calebarrison@gmail.com>
2016-08-08 11:36:25 -0400

    added new negative test

--- multiplication/test_multiply.rb
+++ multiplication/test_multiply.rb
@@ -11,4 +11,10 @@ class TestMultiply < Minitest::Test
 		assert_equal(10, my_multiply(2, 5))
 	end
 
+	def test_2_times_negative_10_equals_negative_20
+		assert_equal(-20, my_multiply(2, -10))
+	
+	end
+
+
 end
